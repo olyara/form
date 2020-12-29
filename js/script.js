2a022087f639a5f7207f383a9adbc1e1afd9bb0f
olyara <olyarakuta@gmail.com>
2020-12-29 18:07:16 +0200

    corect mistakes; add add new, edit, cancel buttons logic for forms info

--- js/script.js
+++ js/script.js
@@ -2,27 +2,39 @@
 
 $(document).ready(function () {
 
-    const profileInfoForm = $("form.form_profile-info").get(0),
-        addressInfoForm = $("form.form_address-info").get(0);
+    const profileInfoForm = $("form.form_profile-info"),
+        addressInfoForm = $("form.form_address-info");
 
     const title = $(profileInfoForm).find("select#title").get(0),
-        inputsFrom1stForm = $(profileInfoForm).find("input[type!='submit']"),
+        inputsFrom1stForm = $(profileInfoForm).find("input.required"),
+        inputsFrom2dForm = $(addressInfoForm).find("input.required"),
         privacy = $(profileInfoForm).find("input#privacy").get(0),
         fname = $("input#fname").get(0),
         lname = $("input#lname").get(0),
         tel = $("input#tel").get(0),
         email = $("input#email").get(0),
+        password = $("input#password").get(0),
         street = $("input#street").get(0),
+        apt = $("input#apt").get(0),
         city = $("input#city").get(0),
+        country = $("input#country").get(0),
         postalCode = $("input#postal_code").get(0);
+
 
     const welcomeMessageProfile = $(".welcome_message_profile"),
         welcomeMessageAddress = $(".welcome_message_address");
 
+    const editButtonProfile = $(".edit-button_profile").get(0), addButtonProfile = $(".add-button_profile").get(0), cancelButtonProfile = $(".cancel-button_profile").get(0);
+    const editButtonAddress = $(".edit-button_address").get(0), addButtonAddress = $(".add-button_address").get(0), cancelButtonAddress = $(".cancel-button_address").get(0);
 
-    function addErrorMessage(field, textMessage) {
+    let objProfile = {}, objAddress = {};
+
+    console.log($(title));
+
+
+
+    function addErrorMessage(field) {
         $(field).siblings(".error_message").css("visibility", "visible");
-        $(field).siblings(".error_message").text(textMessage);
         $(field).css("border", "1.5px solid #da3535");
         $(field).css("padding", "3.5px 8.5px");
     }
@@ -37,61 +49,43 @@ $(document).ready(function () {
     // check if blank before submit
     $(title).on("focusout", function () {
         if ($(this).val() === null) {
-            addErrorMessage(title, "Obligatory field. Please choose from the variants");
+            addErrorMessage(title);
         } else {
             deleteErrorMessage(title);
         }
     });
 
-    $(privacy).on("focusout", function () {
-        if ($(this).prop("checked") === false) {
-            addErrorMessage($(this), "Obligatory field. Please check the box.");
+
+    // inputs
+    $(inputsFrom1stForm).on("focusout", function () {
+        if (this.value === "") {
+            addErrorMessage($(this));
         } else {
             deleteErrorMessage($(this));
         }
     });
 
-    // inputs w/o privacy checkbox
-    const inputs = [];
-    $(inputsFrom1stForm).each(function (index, item) {
-        if (item.type !== "checkbox") {
-            inputs.push(item);
+    $(privacy).on("focusout", function () {
+        if ($(this).prop("checked") === false) {
+            addErrorMessage($(this));
+        } else {
+            deleteErrorMessage($(this));
         }
-    });
-
-    $(inputs).each(function (index, input) {
-
-        $(input).on("focusout", function () {
-            if (input.value === "") {
-                addErrorMessage($(this), "Obligatory field. Please fill it out.");
-            } else {
-                deleteErrorMessage($(this));
-            }
-        });
     });
 
     // check if unnecessary characters are present in fields
-    $(fname).on("change", function (e) {
+    const alphabeticFields = $(profileInfoForm).find("input.alphabetic");
+    $(alphabeticFields).on("change", function (e) {
         let newValue = "";
-        for (let i = 0; i < fname.value.length; i++) {
+        for (let i = 0; i < this.value.length; i++) {
 
-            if ($.isNumeric(fname.value[i]) === false) {
-                newValue += `${fname.value[i]}`;
+            if ($.isNumeric(this.value[i]) === false) {
+                newValue += `${this.value[i]}`;
             }
         }
-        fname.value = newValue;
+        this.value = newValue;
     });
 
-    $(lname).on("change", function () {
-        let newValue = "";
-        for (let i = 0; i < lname.value.length; i++) {
-
-            if ($.isNumeric(lname.value[i]) === false) {
-                newValue += `${lname.value[i]}`;
-            }
-        }
-        lname.value = newValue;
-    });
 
     $(tel).on("change", function () {
         let newValue = "";
@@ -104,14 +98,13 @@ $(document).ready(function () {
         tel.value = newValue;
     });
 
-    $(email).on("keypress", function (e) {
-        if (e.which == 13) {
-            if (/^([A-Za-z0-9_\-.])+@([A-Za-z0-9_\-.])+\.([A-Za-z]{2,4})$/.test(email.value)) {
-                deleteErrorMessage($(email));
-            } else {
-                addErrorMessage($(email), "Please enter a valid e-mail address.");
-            }
+    $(email).on("focusout", function () {
+        if (/^([A-Za-z0-9_\-.])+@([A-Za-z0-9_\-.])+\.([A-Za-z]{2,4})$/.test(this.value)) {
+            deleteErrorMessage(this);
+        } else {
+            addErrorMessage(this);
         }
+
     });
     // / inputs
 
@@ -123,29 +116,28 @@ $(document).ready(function () {
         event.preventDefault();
         // blank
         if ($(title).val() === null) {
-            addErrorMessage(title, "Obligatory field. Please choose from the variants");
+            addErrorMessage(title);
             showWelcomeMessage = false;
         } else {
             deleteErrorMessage(title);
         }
 
-        if ($(privacy).prop("checked") === false) {
-            addErrorMessage($(privacy), "Obligatory field. Please check the box.");
-
-        } else {
-            deleteErrorMessage($(privacy));
-
-        }
-
-        $(inputs).each(function (index, input) {
+        $(inputsFrom1stForm).each(function (index, input) {
             if (input.value === "") {
-                addErrorMessage($(this), "Obligatory field. Please fill it out.");
+                addErrorMessage($(this));
                 showWelcomeMessage = false;
-
             } else {
                 deleteErrorMessage($(this));
             }
         });
+
+
+        if ($(privacy).prop("checked") === false) {
+            addErrorMessage($(privacy));
+            showWelcomeMessage = false;
+        } else {
+            deleteErrorMessage($(privacy));
+        }
 
 
 
@@ -157,36 +149,29 @@ $(document).ready(function () {
                 <br> Last name: ${lname.value}. 
                 <br> Contact phone number: ${tel.value}.
                 `);
-            $(welcomeMessageProfile).css("visibility", "visible");
-            setTimeout(() => {
-                $(welcomeMessageProfile).css("visibility", "hidden");
-            }, 4000);
+            $(welcomeMessageProfile).css("display", "block");
+            $(editButtonProfile).css("display", "block");
+            $(addButtonProfile).css("display", "block");
+            $(this).css("display", "none");
+            objProfile = {
+                title: $(title).val(),
+                firstName: fname.value,
+                lastName: lname.value,
+                telephone: tel.value,
+                email: email.value,
+                password: password.value,
+                privacy: $(privacy).prop("checked"),
+            };
+            let serialObjProfile = JSON.stringify(objProfile);
+            localStorage.setItem("Profile info", serialObjProfile);
         }
-
 
     });
 
 
-
-    $(street).on("focusout", function () {
-        if (street.value === "") {
-            addErrorMessage($(this), "Obligatory field. Please fill it out.");
-        } else {
-            deleteErrorMessage($(this));
-        }
-    });
-
-    $(city).on("focusout", function () {
-        if (city.value === "") {
-            addErrorMessage($(this), "Obligatory field. Please fill it out.");
-        } else {
-            deleteErrorMessage($(this));
-        }
-    });
-
-    $(postalCode).on("focusout", function () {
-        if (postalCode.value === "") {
-            addErrorMessage($(this), "Obligatory field. Please fill it out.");
+    $(inputsFrom2dForm).on("focusout", function () {
+        if (this.value === "") {
+            addErrorMessage($(this));
         } else {
             deleteErrorMessage($(this));
         }
@@ -199,21 +184,21 @@ $(document).ready(function () {
         event.preventDefault();
         // blank
         if (street.value === "") {
-            addErrorMessage($(street), "Obligatory field. Please fill it out.");
+            addErrorMessage($(street));
             showWelcomeMessage = false;
         } else {
             deleteErrorMessage($(street));
         }
 
         if (city.value === "") {
-            addErrorMessage($(city), "Obligatory field. Please fill it out.");
+            addErrorMessage($(city));
             showWelcomeMessage = false;
         } else {
             deleteErrorMessage($(city));
         }
 
         if (postalCode.value === "") {
-            addErrorMessage($(postalCode), "Obligatory field. Please fill it out.");
+            addErrorMessage($(postalCode));
             showWelcomeMessage = false;
         } else {
             deleteErrorMessage($(postalCode));
@@ -223,13 +208,71 @@ $(document).ready(function () {
 
         if (showWelcomeMessage) {
             $(welcomeMessageAddress).html(`Your address (${street.value} Street, ${city.value}, ${postalCode.value}) has been added.`);
-            $(welcomeMessageAddress).css("visibility", "visible");
-            setTimeout(() => {
-                $(welcomeMessageAddress).css("visibility", "hidden");
-            }, 2000);
+            $(welcomeMessageAddress).css("display", "block");
+            $(editButtonAddress).css("display", "block");
+            $(addButtonAddress).css("display", "block");
+            $(this).css("display", "none");
+            objAddress = {
+                street: street.value,
+                apartment: apt.value,
+                city: city.value,
+                country: country.value,
+                postalCode: postalCode.value,
+            };
+            let serialObjAddress = JSON.stringify(objAddress);
+            localStorage.setItem("Address info", serialObjAddress);
         }
-
-
     });
+
+
+    $(addButtonProfile).on("click", function () {
+        $(profileInfoForm).css("display", "block");
+        $(title).find("option:first").prop("selected", true);
+        $(inputsFrom1stForm).each(function (index, input) {
+            input.value = "";
+        });
+        $(privacy).prop("checked", false);
+    });
+
+    $(editButtonProfile).on("click", function () {
+        $(profileInfoForm).css("display", "block");
+        $(cancelButtonProfile).css("display", "inline");
+    });
+
+    $(cancelButtonProfile).on("click", function () {
+        let returnObjProfile = JSON.parse(localStorage.getItem("Profile info"));
+        $(title).val(returnObjProfile.title);
+        fname.value = returnObjProfile.firstName;
+        lname.value = returnObjProfile.lastName;
+        tel.value = returnObjProfile.telephone;
+        email.value = returnObjProfile.email;
+        password.value = returnObjProfile.password;
+        $(privacy).prop("checked", returnObjProfile.privacy);
+    });
+
+
+
+    $(addButtonAddress).on("click", function () {
+        $(addressInfoForm).css("display", "block");
+        $(inputsFrom2dForm).each(function (index, input) {
+            input.value = "";
+        });
+        showWelcomeMessage = true;
+    });
+
+    $(editButtonAddress).on("click", function () {
+        $(addressInfoForm).css("display", "block");
+        $(cancelButtonAddress).css("display", "inline");
+    });
+
+    $(cancelButtonAddress).on("click", function () {
+        let returnObjAddress = JSON.parse(localStorage.getItem("Address info"));
+        street.value = returnObjAddress.street;
+        apt.value = returnObjAddress.apartment;
+        city.value = returnObjAddress.city;
+        country.value = returnObjAddress.country;
+        postalCode.value = returnObjAddress.postalCode;
+    });
+
 });
 
